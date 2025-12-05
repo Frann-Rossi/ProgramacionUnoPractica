bb882d67728c777cf36050197be5dc171c209425
Frann-Rossi <rossifrancomatias@gmail.com>
2025-12-05 17:54:52 -0300

    hasta ej 4 de struct

---
+++ Unidad4/TP6/main.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_NOMBRE 30
+#define MAX_ALUMNOS 50
+
+typedef struct
+{
+    int matricula;
+    char nombre[MAX_NOMBRE];
+    char genero; //m, f, o
+} stAlumno;
+
+stAlumno cargarAlumno ()
+{
+    stAlumno alum;
+    printf("Matricula:");
+    scanf("%d",&alum.matricula);
+    printf("\nNombre:");
+    fflush(stdin);
+    scanf("%s",alum.nombre);
+    //gets(alum->nombre);
+    printf("\nGenero:");
+    fflush(stdin);
+    scanf("%c",&alum.genero);
+    return alum;
+}
+
+int cargarAlumnos(stAlumno arrAlum[], int dim)
+{
+    int i = 0;
+    char control = 's';
+
+    while(control == 's' && i < dim )
+    {
+        arrAlum[i] = cargarAlumno();
+        i++;
+        printf("Desea seguir cargando ALUMNOS 's/n':");
+        fflush(stdin);
+        scanf("%c",&control);
+    }
+    return i;
+}
+
+void mostrarAlumno(stAlumno alum)
+{
+    printf("|Matricula:%d|//|Nombre:%s||Genero:%c|\n",alum.matricula,alum.nombre,alum.genero);
+}
+
+void mostrarAlumnos(stAlumno arrAlum[],int val)
+{
+    for(int i = 0; i < val; i++)
+    {
+        mostrarAlumno(arrAlum[i]);
+    }
+}
+
+int buscarMatricula (stAlumno arrAlum[], int val, int numBuscado)
+{
+    int flag = -1;
+    for(int i = 0; i < val ; i++)
+    {
+        if(arrAlum[i].matricula == numBuscado)
+        {
+            flag = i;
+        }
+    }
+    return flag;
+}
+
+void mostrarAlumnoPorMatricula (stAlumno arrAlum[],int val)
+{
+    int posicion;
+    int numBuscado;
+    printf("Ingrese matricula a buscar:");
+    scanf("%d",&numBuscado);
+
+    posicion = buscarMatricula(arrAlum,val,numBuscado);
+
+    if(posicion != -1)
+    {
+        printf("\nAlumno Encontrado:\n");
+        mostrarAlumno(arrAlum[posicion]);
+    }
+    else
+    {
+        printf("No se encontro ningun alumno con esa matricula.\n");
+    }
+}
+
+int buscarMenor(stAlumno arrAlum[],int val, int posInicial)
+{
+    int posMenor = posInicial;
+    for(int i = posInicial + 1; i < val; i++)
+    {
+        if(arrAlum[i].matricula < arrAlum[posMenor].matricula)
+        {
+            posMenor = i;
+        }
+    }
+    return posMenor;
+}
+
+void ordenamientoPorSeleccion (stAlumno arrAlum[],int val)
+{
+    int posMenor;
+    stAlumno aux;
+    for(int i = 0; i < val - 1; i++)
+    {
+        posMenor = buscarMenor(arrAlum,val,i);
+        aux = arrAlum[posMenor];
+        arrAlum[posMenor] = arrAlum[i];
+        arrAlum[i] = aux;
+
+    }
+}
+
+int main()
+{
+    char control = 's';
+    int opcion;
+
+    stAlumno arrAlumnos[MAX_ALUMNOS];
+    int val = 0;
+    while(control == 's')
+    {
+        printf("\n\n--- TRABAJO PRACTICO 6: ESTRUCTURAS ---\n");
+        printf("1. Cargar arreglo de alumnos\n");
+        printf("2. Mostrar arreglo de alumnos\n");
+        printf("3. Buscar alumno por matricula\n");
+        printf("4. Ordenar por matricula (Seleccion)\n");
+        printf("5. Mostrar alumnos de un genero\n");
+        printf("6. Insertar alumno en arreglo ordenado (por matricula)\n");
+        printf("7. Ordenar por nombre (Insercion)\n");
+        printf("8. Contar estudiantes de un genero\n");
+        printf("0. Salir\n");
+        printf("Ingrese una opcion: ");
+        scanf("%d", &opcion);
+
+        switch(opcion)
+        {
+        case 1:
+            val = cargarAlumnos(arrAlumnos,MAX_ALUMNOS);
+            printf("Se cargaron %d alumnos\n",val);
+            break;
+        case  2:
+            mostrarAlumnos(arrAlumnos,val);
+            break;
+        case 3:
+            mostrarAlumnoPorMatricula(arrAlumnos,val);
+            break;
+        case 4:
+            ordenamientoPorSeleccion(arrAlumnos,val);
+            mostrarAlumnos(arrAlumnos,val);
+            break;
+        case 0:
+            printf("Saliendo del programa...\n");
+            control = 'n';
+            break;
+        default:
+            printf("Ingrese un valor valido\n");
+        }
+
+        if(control == 's')
+        {
+            printf("Desea seguir en el programa 's/n':");
+            fflush(stdin);
+            scanf("%c",&control);
+        }
+    }
+    return 0;
+}
