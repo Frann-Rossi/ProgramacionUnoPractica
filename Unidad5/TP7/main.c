8e189ba1c2385debe269a6d6f8cb01f2607571d5
Frann-Rossi <rossifrancomatias@gmail.com>
2025-12-08 12:44:52 -0300

    tp archivos terminado

--- Unidad5/TP7/main.c
+++ Unidad5/TP7/main.c
@@ -4,6 +4,7 @@
 #include "pila.h"
 
 #define MAX_NOMBRE 30
+#define MAX_ALUMNOS 30
 
 typedef struct
 {
@@ -180,7 +181,7 @@ void pasarLegajosAPila (char nombreArchivo[], Pila *origen)
     }
     else
     {
-        printf("Error al abrir el archivo");
+        printf("Error al abrir el archivo\n");
     }
 }
 
@@ -203,7 +204,7 @@ int contarMayoresEdad(char nombreArchivo[], int edadLimite)
     }
     else
     {
-        printf("Error al abrir el archivo");
+        printf("Error al abrir el archivo\n");
     }
 
     return cont;
@@ -237,6 +238,253 @@ void mostrarNombresPorEdad (char nombreArchivo[],int min, int max)
     }
 }
 
+stAlumno buscarAlumnoMayor(char nombreArchivo[])
+{
+    stAlumno alum;
+    stAlumno alumMayor;
+    alumMayor.edad = -1;
+    FILE *buffer = fopen(nombreArchivo,"rb");
+
+    if(buffer)
+    {
+        while(fread(&alum,sizeof(stAlumno),1,buffer) > 0)
+        {
+            if(alumMayor.edad == -1 || alum.edad > alumMayor.edad)
+            {
+                alumMayor = alum;
+            }
+        }
+        fclose(buffer);
+    }
+    else
+    {
+        printf("Error al abrir el archivo\n");
+    }
+    return alumMayor;
+
+}
+
+void mostrarMayorDelArchivo(char nombreArchivo[])
+{
+    stAlumno alum;
+    alum = buscarAlumnoMayor(nombreArchivo);
+    if(alum.edad != -1)
+    {
+        printf("\n--- ALUMNO CON MAYOR EDAD ---\n");
+        mostrarAlum(alum);
+    }
+    else
+    {
+        printf("No se pudo determinar el alum (archivo vacio o error).\n");
+    }
+}
+
+int cantDeAlumDeUnAnio(char nombreArchivo[],int anioBuscado)
+{
+    stAlumno alum;
+    int cant = 0;
+    FILE *buffer = fopen(nombreArchivo,"rb");
+
+    if(buffer)
+    {
+        while(fread(&alum, sizeof(stAlumno), 1, buffer) > 0)
+        {
+            if(alum.anio == anioBuscado)
+            {
+                cant++;
+            }
+        }
+        fclose(buffer);
+    }
+    else
+    {
+        printf("Error al abrir el archivo\n");
+    }
+    return cant;
+}
+
+int cargarAlumnos(stAlumno arrAlum[], int dim)
+{
+    int i = 0 ;
+    char control = 's';
+
+    while(control == 's' && i < dim)
+    {
+        arrAlum[i] = cargarAlum();
+        i++;
+        printf("Desea seguir cargando ALUMNOS en el arr 's/n':");
+        fflush(stdin);
+        scanf("%c",&control);
+    }
+    return i;
+}
+void mostrarAlumnosArr(stAlumno arrAlum[],int val)
+{
+    for(int i = 0; i < val; i++)
+    {
+        mostrarAlum(arrAlum[i]);
+    }
+}
+
+void pasarArrAlumAArchivo (char nombreArchivo[], stAlumno arrAlum[], int val)
+{
+    FILE *buffer = fopen(nombreArchivo,"wb");
+
+    if(buffer)
+    {
+        for(int i = 0; i < val; i++)
+        {
+            fwrite(&arrAlum[i],sizeof(stAlumno),1,buffer);
+        }
+        fclose(buffer);
+    }
+    else
+    {
+        printf("Error al abrir el archivo\n");
+    }
+}
+
+int pasarArchivoAlumAArr (char nombreArchivo [], stAlumno arrAlum[],int anio)
+{
+    stAlumno alum;
+    int i = 0;
+    FILE *buffer = fopen(nombreArchivo,"rb");
+
+    if(buffer)
+    {
+        while(fread(&alum,sizeof(stAlumno),1,buffer)>0)
+        {
+            if(alum.anio == anio)
+            {
+                arrAlum[i] = alum;
+                i++;
+            }
+        }
+        fclose(buffer);
+    }
+    else
+    {
+        printf("Error al abrir el archivo\n");
+    }
+    return i;
+}
+
+int cantDeRegistro (char nombreArchivo[])
+{
+    stAlumno alum;
+    int cant = 0;
+    FILE *buffer = fopen(nombreArchivo,"rb");
+
+    if(buffer)
+    {
+        fseek(buffer,0,SEEK_END);
+        cant = ftell(buffer) / sizeof(stAlumno);
+        fclose(buffer);
+    }
+    else
+    {
+        printf("Error al abrir el archivo\n");
+    }
+    return cant;
+}
+
+void mostrarAlumnoPorPosicion(char nombreArchivo[], int pos)
+{
+    int cant = cantDeRegistro(nombreArchivo);
+    if(pos >= 0 && pos < cant)
+    {
+        stAlumno alum;
+        FILE *buffer = fopen(nombreArchivo,"rb");
+        if(buffer)
+        {
+            fseek(buffer,pos * sizeof(stAlumno),SEEK_SET);
+            fread(&alum,sizeof(stAlumno),1,buffer);
+            printf("\n--- ALUMNO EN POSICION %d ---\n", pos);
+            mostrarAlum(alum);
+            fclose(buffer);
+        }
+    }
+    else
+    {
+        printf("Error al abrir el archivo\n");
+    }
+}
+
+void modificarRegistro(char nombreArchivo[], int pos)
+{
+    int cant =  cantDeRegistro(nombreArchivo);
+    if(pos >= 0 && pos < cant)
+    {
+        stAlumno alum;
+        FILE *buffer = fopen(nombreArchivo,"r+b");
+        if(buffer)
+        {
+            fseek(buffer,pos * sizeof(stAlumno),SEEK_SET);
+            fread(&alum,sizeof(stAlumno),1,buffer);
+            printf("\n--- DATOS ACTUALES ---\n");
+            mostrarAlum(alum);
+            printf("\n--- INGRESE LOS NUEVOS DATOS ---\n");
+            printf("Nuevo Nombre: ");
+            fflush(stdin);
+            gets(alum.nombreYapellido);
+            printf("Nueva Edad: ");
+            scanf("%d", &alum.edad);
+            printf("Nuevo Anio: ");
+            scanf("%d", &alum.anio);
+            fseek(buffer, -1 * (long)sizeof(stAlumno), SEEK_CUR);
+            fwrite(&alum, sizeof(stAlumno), 1, buffer);
+            printf("\nRegistro modificado exitosamente.\n");
+            fclose(buffer);
+        }
+    }
+}
+
+void invertirArchivo(char nombreArchivo[])
+{
+    stAlumno alumInicio;
+    stAlumno alumFinal;
+    int posInicio = 0;
+    int posFinal = cantDeRegistro(nombreArchivo) - 1; // El ultimo es cantidad - 1
+
+    // Abrimos en lectura y escritura
+    FILE *buffer = fopen(nombreArchivo, "r+b");
+
+    if (buffer)
+    {
+        // El bucle va mientras el inicio no se cruce con el final
+        while (posInicio < posFinal)
+        {
+            // 1. Buscamos y leemos el del INICIO
+            fseek(buffer, posInicio * sizeof(stAlumno), SEEK_SET);
+            fread(&alumInicio, sizeof(stAlumno), 1, buffer);
+
+            // 2. Buscamos y leemos el del FINAL
+            fseek(buffer, posFinal * sizeof(stAlumno), SEEK_SET);
+            fread(&alumFinal, sizeof(stAlumno), 1, buffer);
+
+            // 3. Escribimos el del INICIO en la posicion del FINAL
+            // (Ojo: fseek es necesario porque acabamos de leer)
+            fseek(buffer, posFinal * sizeof(stAlumno), SEEK_SET);
+            fwrite(&alumInicio, sizeof(stAlumno), 1, buffer);
+
+            // 4. Escribimos el del FINAL en la posicion del INICIO
+            fseek(buffer, posInicio * sizeof(stAlumno), SEEK_SET);
+            fwrite(&alumFinal, sizeof(stAlumno), 1, buffer);
+
+            // 5. Movemos los punteros hacia el centro
+            posInicio++;
+            posFinal--;
+        }
+
+        fclose(buffer);
+        printf("\nArchivo invertido correctamente!\n");
+    }
+    else
+    {
+        printf("Error al abrir el archivo.\n");
+    }
+}
+
 int main()
 {
     char control = 's';
@@ -248,6 +496,8 @@ int main()
     Pila origen;
     inicpila(&origen);
     int min, max;
+    char arrAlumnos[MAX_ALUMNOS];
+    int val;
 
     while(control == 's')
     {
@@ -312,6 +562,52 @@ int main()
             printf("Ingrese edad maxima: ");
             scanf("%d", &max);
             mostrarNombresPorEdad(archivoAlum, min, max);
+            break;
+        case 10:
+            mostrarMayorDelArchivo(archivoAlum);
+            break;
+        case 11:
+            printf("Ingrese el anio a contar: ");
+            scanf("%d", &num);
+            cant = cantDeAlumDeUnAnio(archivoAlum, num);
+            printf("Hay %d alumnos en el anio %d.\n", cant, num);
+            break;
+        case 12:
+            printf("\n--- CARGA DE ARREGLO ---\n");
+            val = cargarAlumnos(arrAlumnos, MAX_ALUMNOS);
+            printf("\n--- CONTENIDO DEL ARR ---\n");
+            mostrarAlumnosArr(arrAlumnos, val);
+
+            pasarArrAlumAArchivo(archivoAlum, arrAlumnos, val);
+
+            printf("\n--- CONTENIDO DEL ARCHIVO ---\n");
+            mostrarAlumArchivo(archivoAlum);
+
+            printf("\n--- FILTRAR ARCHIVO -> ARREGLO ---\n");
+            printf("Ingrese anio a buscar: ");
+            scanf("%d", &num);
+
+            val = pasarArchivoAlumAArr(archivoAlum, arrAlumnos, num);
+
+            printf("\n--- RESULTADO FINAL EN ARREGLO ---\n");
+            mostrarAlumnosArr(arrAlumnos, val);
+            break;
+        case 13:
+            cant = cantDeRegistro(archivoAlum);
+            printf("Cantidad de Alumnos son:%d\n",cant);
+            break;
+        case 14:
+            printf("Ingrese una POS a buscar: ");
+            scanf("%d", &num);
+            mostrarAlumnoPorPosicion(archivoAlum,num);
+            break;
+        case 15:
+            printf("Ingrese la posicion a modificar: ");
+            scanf("%d", &num);
+            modificarRegistro(archivoAlum, num);
+            break;
+        case 16:
+            invertirArchivo(archivoAlum);
             break;
         case 0:
             printf("Saliendo del programa...\n");
